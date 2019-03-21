13b264d6de2d4021cd2c47c8a4c53f7fb5208f1d
chenchang <chenchang@kezaihui.com>
2019-03-21 15:17:13 +0800

    jdbc

---
+++ src/main/java/cn/tianyu/springjdbc/AccountDaoImpl2.java
@@ -0,0 +1,35 @@
+package cn.tianyu.springjdbc;
+
+import java.util.List;
+
+public class AccountDaoImpl2 extends JdbcDaoSupport implements IAccountDao {
+
+    @Override
+    public Account findAccountById(Integer id) {
+        //getJdbcTemplate()方法是从父类上继承下来的。
+        List<Account> list = getJdbcTemplate().query("select * from account where id = ? ",
+                new AccountRowMapper(),id);
+        return list.isEmpty()?null:list.get(0);
+    }
+
+    @Override
+    public Account findAccountByName(String name) {
+        //getJdbcTemplate()方法是从父类上继承下来的。
+        List<Account> list =  getJdbcTemplate().query("select * from account where name = ? ",
+                new AccountRowMapper(),name);
+        if(list.isEmpty()){
+            return null;
+        }
+        if(list.size()>1){
+            throw new RuntimeException("结果集不唯一，不是只有一个账户对象");
+        }
+        return list.get(0);
+    }
+
+    @Override
+    public void updateAccount(Account account) {
+        //getJdbcTemplate()方法是从父类上继承下来的。
+        getJdbcTemplate().update("update account set money = ? where id = ? ",
+                account.getMoney(),account.getId());
+    }
+}
--- src/main/java/cn/tianyu/springjdbc/Client.java
+++ src/main/java/cn/tianyu/springjdbc/Client.java
@@ -8,9 +8,18 @@ import java.util.List;
 
 public class Client {
     public static void main(String[] args) {
-
-        SearchAll();
+        DaoUsing();
     }
+
+    static void DaoUsing()
+    {
+        //1.获取Spring容器
+        ApplicationContext ac = new ClassPathXmlApplicationContext("springjdbc.xml");
+        IAccountDao dao = (IAccountDao) ac.getBean("accountDaoImpl2");
+        Account act = dao.findAccountByName("aaa");
+        System.out.println(act);
+    }
+
     static void Insert()
     {
         //1.获取Spring容器
@@ -35,4 +44,17 @@ public class Client {
             System.out.println(o);
         }
     }
+    static void SearchOne()
+    {
+        //1.获取Spring容器
+        ApplicationContext ac = new ClassPathXmlApplicationContext("springjdbc.xml");
+        //2.根据id获取bean对象
+        JdbcTemplate jt = (JdbcTemplate) ac.getBean("jdbcTemplate");
+        //3.执行操作
+        //查询一个
+        List<Account> as = jt.query("select * from account where id = ? ",
+                new AccountRowMapper(), 2);
+        System.out.println(as.isEmpty()?"没有结果":as.get(0));
+    }
+
 }
